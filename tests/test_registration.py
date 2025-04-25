24c14d9cf8bbfdccd2e0f13f51bcf2e06cf851d7
Natalia Yakunina <pkr-ek@yandex.ru>
2025-04-25 12:28:41 +0500

    Added test_cannot_register_with_an_invalid_email

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from curl import main_site
 from helper import generate_registration_data
 from helper import generate_registration_data_without_a_dot_in_an_email
+from helper import generate_registration_data_with_invalid_email_missing_at
 from locators import Locators
 
 
@@ -55,4 +56,14 @@ class TestRegistrationWithNewData:
 
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/register'
 
+    def test_cannot_register_with_an_invalid_email_missing_at(self,driver):
 
+        name, invalid_email, password = generate_registration_data_with_invalid_email_missing_at()
+        driver.find_element(*Locators.LOGIN_BUTTON).click()
+        driver.find_element(*Locators.REGISTER_LINK).click()
+        driver.find_element(*Locators.NAME_INPUT).send_keys(name)
+        driver.find_element(*Locators.EMAIL_INPUT).send_keys(invalid_email)
+        driver.find_element(*Locators.PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+
+        assert driver.current_url == 'https://stellarburgers.nomoreparties.site/register'
